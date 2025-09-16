311d55321b16b947727d8dad7ddcd0813593e593
kapusta <anddey09@gmail.com>
2025-09-16 13:56:47 +0700

    нормааÐльное ÐддвиÐвижееÐниеÐ окна

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import tkinter as tk
 
 root = tk.Tk()
-
 root.overrideredirect(True)
-
 root.attributes('-topmost', True)
-
 root.attributes('-alpha', 0.9)
-
 root.geometry("400x300")
-
 root.configure(bg='#2c2c2c')
+
 
 def add_task():
     text = entry.get().strip()
@@ -19,6 +15,29 @@ def add_task():
         entry.delete(0, tk.END)
     else:
         print("Введите задачу")
+
+def start_move(event):
+    global offset_x, offset_y
+    offset_x = event.x
+    offset_y = event.y
+
+def do_move(event):
+    new_x = event.x_root - offset_x
+    new_y = event.y_root - offset_y
+    root.geometry(f'+{new_x}+{new_y}')
+
+
+title_bar = tk.Label(
+    root,
+    text="☰  План дел",
+    bg='#3a3a3a',
+    fg='white',
+    font=("Arial", 10, "bold"),
+    )
+
+title_bar.pack(fill=tk.X, padx=5, pady=(5, 2))
+root.bind('<Button-1>', start_move)
+root.bind('<B1-Motion>', do_move)
 
 
 close_btn = tk.Button(
@@ -53,6 +72,7 @@ add_btn = tk.Button(
     relief='flat'
 )
 add_btn.place(x=0, y=5)
+
 
 root.mainloop()
 
